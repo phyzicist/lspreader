d8842b4dd1a0e5da93b35d32dc305eff214ee997
noobermin <ngirmang.1@osu.ed>
2014-07-16 18:37:05 -0400

    Added optional limits for plotmat.py

--- plotmat.py
+++ plotmat.py
@@ -10,7 +10,7 @@ from docopt import docopt;
 usage='''Plot cutplane at center
 
 Usage:
-  ./plotmat.py <infile> <outfile> <time>
+  ./plotmat.py [--min=MIN --max=MAX] <infile> <outfile> <time>
 '''
 
 def read_file(filename):
@@ -30,12 +30,21 @@ def zero_nan(S):
 
 def main():
     opts=docopt(usage,help=True);
+    if opts['min']:
+        vmin = float(opts['min']);
+    else:
+        vmin = -1.0;
+    if opts['max']:
+        vmax = float(opts['max']);
+    else:
+        vmax = 23.5;
+        
     S = read_file(opts['<infile>']);
     S = zero_nan(S);
     X,Y = np.mgrid[-20:20:100j,-30:5:100j];
-    plt.pcolormesh(X,Y,S[:,50,:],vmin=-1.0,vmax=23.5);
-    plt.xlabel('x ($\mu m$)');
-    plt.ylabel('z ($\mu m$)');
+    plt.pcolormesh(X,Y,S[:,50,:],vmin=vmin,vmax=vmax);
+    plt.xlabel('z ($\mu m$)');
+    plt.ylabel('x ($\mu m$)');
     plt.annotate('t = {} fs'.format(opts['<time>']),(-19,3),color='w');
     c=plt.colorbar();
     c.set_label('log10 of density');
