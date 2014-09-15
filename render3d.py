3da26407d359e3b168f8e42aad157a6a49a22904
noobermin <ngirmang.1@osu.edu>
2014-09-15 10:12:41 -0400

    changed render3d.py to include zeroing of parts.

--- render3d.py
+++ render3d.py
@@ -4,7 +4,8 @@ import cPickle;
 from docopt import docopt;
 import re;
 import math;
-usage='''Render volumetric render of a scalar field
+from misc import conv;
+usage='''Render volumetric render of a scalar field.
 
 Usage:
   render3d.py [options] IN_FORMAT OUT_FORMAT LABEL_FORMAT <lownum> <highnum>
@@ -16,6 +17,11 @@ Options:
    --plot-single -s             Just show, don't output, single scalar.
    --cutplane=Z                 Make cutplane along z at Z.
    --clabel=CLABEL              Set the colorbar label.
+   --zero-x=XRANGE              Set a range of x to zero, as a python tuple.
+   --zero-y=YRANGE              Set a range of y to zero, as a python tuple.
+   --zero-z=ZRANGE              Set a range of z to zero, as a python tuple.
+   --azimuth=AZIMUTH            Set the azimuth.
+   --polar=POLAR                Set the polar angle.
 '''
 
 def logprint(s):
@@ -36,14 +42,23 @@ def tozero(v):
 def zero_nan(S):
     return np.array([[[tozero(k) for k in j] for j in i] for i in S]);
 
-def initial_plot(mlab,fname,vlim,
-                 clabel=None,cutplane=None,label=None):
+def zero_range(S,zeros):
+    if zeros == [None,None,None]:
+        print('doing nothing');
+        return S;
+    zeros[:] =[ (None,None) if i is None else i for i in zeros];
+    S[zeros[0][0]:zeros[0][1],zeros[1][0]:zeros[1][1],zeros[2][0]:zeros[2][1]]=0.0;
+    return S;
+
+def initial_plot(mlab,fname,vlim,angle,
+                 clabel=None,cutplane=None,label=None,zeros=[None,None,None]):
     '''
     Creates the first plot.
     '''
     ret = {};
     S=read_file(fname);
-    S=np.log10(zero_nan(S)+0.1);
+    S=zero_range(zero_nan(S),zeros)
+    S=np.log10(S+0.1);
     fig=mlab.figure(size=(1280,1024));
     ret['fig']=fig;
     fig.scene.disable_render=True;
@@ -58,7 +73,7 @@ def initial_plot(mlab,fname,vlim,
                                             vmin=vlim[0],vmax=vlim[1]);
         ret['zp'] = zp;
         
-    mlab.view(azimuth=150,elevation=45,focalpoint='auto',distance='auto');
+    mlab.view(elevation=angle[0],azimuth=angle[1],focalpoint='auto',distance='auto');
     mlab.scalarbar(object=v,title=clabel);
     #Unfortunately, the volume renderer doesn't work out of the box.
     #The main issue is the colorbar. So, we do this instead.
@@ -73,22 +88,24 @@ def initial_plot(mlab,fname,vlim,
     fig.scene.disable_render=False;
     return ret;
 
-def plot(names,vlim,
-         clabel=None,cutplane=None):
+def plot(names,vlim,angle,
+         clabel=None,cutplane=None,zeros=[None,None,None]):
     print("loading mlab");
     import mayavi.mlab as mlab;
     mlab.options.offscreen = True;
     outname,inname,label = names[0];
     names = names[1:];
     print("processing first file {}...".format(inname));
-    d=initial_plot(mlab,inname,vlim,clabel=clabel,
-                   cutplane=cutplane,label=label);
+    d=initial_plot(mlab,inname,vlim,angle,
+                   clabel=clabel,cutplane=cutplane,label=label,zeros=zeros);
     print("saving {}".format(outname));
     mlab.savefig(outname,size=(1280,1024));
     for outname,inname,label in names:
         print("processing {}...".format(inname))
         S=read_file(inname);
-        S=np.log10(zero_nan(S)+0.1);
+        S=zero_range(zero_nan(S),zeros)
+        S=np.log10(S+0.1);
+        S=zero_range(S,zeros);
         d['fig'].scene.disable_render=True;
         d['v'].mlab_source.set(scalars=S);
         if cutplane:
@@ -99,13 +116,13 @@ def plot(names,vlim,
         mlab.savefig(outname,size=(1280,1024));
     pass
 
-
-
-def plot_single(inname,vlim,
-                clabel=None,cutplane=None):
+def plot_single(inname,vlim,angle,
+                clabel=None,cutplane=None,
+                zeros=[None,None,None],):
     print("loading mlab");
     import mayavi.mlab as mlab;
-    initial_plot(mlab,inname,vlim,cutplane=cutplane);
+    initial_plot(mlab,inname,vlim,angle,
+                 cutplane=cutplane,clabel=clabel,zeros=zeros);
     print('plotting');
     mlab.show();
 
@@ -114,6 +131,11 @@ if __name__=="__main__":
     opts=docopt(usage,help=True);
     vmin = float(opts['--min']) if opts['--min'] else -1;
     vmax = float(opts['--max']) if opts['--max'] else 23.5;
+    zeros = [conv(opts['--zero-x'],func=eval,default=None),
+             conv(opts['--zero-y'],func=eval,default=None),
+             conv(opts['--zero-z'],func=eval,default=None)];
+    angle = [conv(opts['--polar'],func=float,default=45),
+             conv(opts['--azimuth'],func=float,default=160)]
     clabel = opts['--clabel'] if opts['--clabel'] else 'log10 of electron density';
     if not opts['--plot-single']:
         if '*' not in opts['IN_FORMAT'] or '*' not in opts['OUT_FORMAT']:
@@ -130,8 +152,12 @@ if __name__=="__main__":
         innames = [in_fmt.format(i) for i in numrange];
         labels = [label_fmt.format(i) for i in numrange];
         files = zip(outnames,innames,labels);
-        plot(files,(vmin,vmax),cutplane=opts['--cutplane'],clabel=clabel);
+        plot(files,(vmin,vmax),
+             cutplane=opts['--cutplane'],clabel=clabel,
+             zeros=zeros,angle=angle);
     else:
-        plot_single(opts['INFILE'],(vmin,vmax),cutplane=opts['--cutplane'],clabel=clabel);
+        plot_single(opts['INFILE'],(vmin,vmax),angle,
+                    cutplane=opts['--cutplane'],clabel=clabel,
+                    zeros=zeros);
     pass;
 pass;
