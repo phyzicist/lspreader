4100ec1b15019ed548d3097ffa4da9511c1c42a0
Gregory Ngirmang <ngirmang.1@osu.edu>
2015-10-31 23:42:18 -0400

    added reader to lspreader2

--- pmov-simple.py
+++ pmov-simple.py
@@ -5,13 +5,16 @@ Extract pmovie particles.
 Usage:
   pmovie.py <input> <output>
 '''
-import lspreader as rd;
+import lspreader2 as rd;
 import cPickle as pickle
 from docopt import docopt;
 
+def _print(s):
+    print(s);
+vprint = _print if opts['--verbose'] else lambda s:None;
+
 opts = docopt(__doc__,help=True);
-with rd.LspOutput(opts['<input>']) as f:
-    d=f._getmovie();
+d=rd.read(opts['<input>']);
 with open(opts['<output>'],'w') as f:
     pickle.dump(d,f,2);
 
