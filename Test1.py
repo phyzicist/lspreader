d1a0d5fea1af559831a2e2abb162e4c6a3910cb3
Scott Feister <sfeister@gmail.com>
2015-12-29 12:16:52 -0500

    Added a second fields reader to lsp_reader2 library

---
+++ Test1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 28 15:30:36 2015
+
+@author: Scott
+"""
+
+# A test of the lspreader
+
+#import h5py
+import lspreader2 as rd
+
+fn = r"C:\Users\Scott\Documents\temp\lsp laser test fields\flds1330.p4"
+
+data, header, doms = rd.read_flds2(fn, flds=['E','B'])
