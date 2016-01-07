5426750e178cfa18438008965f4940bf580974af
Scott Feister <sfeister@gmail.com>
2016-01-07 18:23:26 -0500

    Updated field stitch outputs, frequency analysis

---
+++ freqanalysis.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Analyze a batch of p4 files for frequency components
+
+Created on Wed Dec 30 15:09:37 2015
+
+@author: Scott
+"""
+import h5py
+import os
+import lspreader2 as rd
+import numpy as np
+from h5stitch2D import chunkIt, getfnsp4, getfns, fields2D, h5fields2D, h5fields2Dpar2, getTimes
+import gzip
+import matplotlib.pyplot as plt
+
+
+## MAIN PROGRAM
+folder = r'/media/sf_temp/lastest/run2_lastest longer/gzipdir'
+
+fns = getfnsp4(folder)[::2]
+#h5fields2D(folder, fld_ids = ['Ez'])
+#h5fields2Dpar2(folder, fld_ids = ['Ez'])
+
+# Load in the data
+divsp = 1 # Integer divisor by which to reduce the spatial resolution
+data = fields2D(fns, fld_ids = ['Ez'])
+times = data['times']
+fns = data['filenames']
+xgv = data['xgv'][::divsp]*1e4
+zgv = data['zgv'][::divsp]*1e4
+Ez = data['Ez'][:,::divsp,::divsp]
+
+# Assume equal spacing in time and space, and get the deltas
+dt = np.mean(np.diff(times))
+dx = np.mean(np.diff(xgv))
+dz = np.mean(np.diff(zgv))
+
+print dx, dz, dt
+
+# Calculate the frequency of the laser from its wavelength (800 nm)
+c = 3e8 # Speed of light in m/s
+wl = 0.8e-6 # Wavelength of laser in m
+fr_fund = c/wl # Frequency of the laser (the 'fundamental'), in Hz
+
+freqHz = np.fft.rfftfreq(len(times), d = dt/1e9) # Frequencies in Hz, for upcoming real FFT
+freq = freqHz/fr_fund # Frequencies in units of the fundamental
+
+# Make some interesting maps
+Imap = np.zeros((Ez.shape[1],Ez.shape[2])) # An intensity map
+FTmap1 = np.zeros((Ez.shape[1],Ez.shape[2])) # An intensity map, but only over certain frequencies
+FTmap2 = np.zeros((Ez.shape[1],Ez.shape[2])) # An intensity map, but only over certain frequencies
+pwr_sum = np.zeros(freq.shape)
+
+map1_condit = np.logical_and(freq > 0.9, freq < 1.1)
+map2_condit = np.logical_and(freq > 0.3, freq < 0.7)
+
+for i in range(len(zgv)):
+    for j in range(len(xgv)):
+        tline = Ez[:,i,j]
+        
+        # Make a simple intensity map        
+        esum = np.sum(tline**2)
+        Imap[i,j] = esum
+        
+        # Make a more complex ftransform map
+        time_ft = np.fft.rfft(tline)
+        pwr = np.abs(time_ft)**2
+        FTmap1[i,j] = np.mean(pwr[map1_condit])
+        FTmap2[i,j] = np.mean(pwr[map2_condit])
+        pwr_sum = pwr_sum + pwr;
+
+
+# Make some plots
+figs = [None]*4
+# Time-integrated intensity map (a.u.)
+figs[0] = plt.figure(1)
+plt.clf() # Clear the figure
+ax = plt.subplot(111)
+ax.pcolorfast(xgv,zgv,Imap, cmap='gray')
+ax.set_xlabel('X (um)')
+ax.set_ylabel('Z (um)')
+ax.set_title('Integrated power from Ez (a.u.)')
+X,Z = np.meshgrid(xgv,zgv)
+plt.contour(X,Z,Imap)
+
+# Fourier transform subset map 1
+figs[1] = plt.figure(2)
+plt.clf() # Clear the figure
+ax = plt.subplot(111)
+ax.pcolorfast(xgv,zgv,FTmap1, cmap='gray')
+ax.set_xlabel('X (um)')
+ax.set_ylabel('Z (um)')
+ax.set_title('Fundamental map (0.9 to 1.1 x fundamental) (a.u.)')
+
+# Fourier transform subset map 2
+figs[2] = plt.figure(3)
+plt.clf() # Clear the figure
+ax = plt.subplot(111)
+ax.pcolorfast(xgv,zgv,FTmap2, cmap='gray')
+ax.set_xlabel('X (um)')
+ax.set_ylabel('Z (um)')
+ax.set_title('1/2-omega map (0.3 to 0.7 x fundamental) (a.u.)')
+
+
+# Average power
+figs[3] = plt.figure(4)
+plt.clf() # Clear the figure
+ax = plt.subplot(111)
+ax.plot(freq, np.log10(pwr_sum))
+ax.set_xlabel('Frequency (normalized to laser fundamental)')
+ax.set_ylabel('Log10(Power spectrum) (a.u.)')
+ax.set_title('Power spectrum (a.u.)')
+
+for i in range(len(figs)):
+    figs[i].savefig('fig' + str(i) + '.png')
