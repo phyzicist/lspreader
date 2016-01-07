41c58ddb850ee05530500add8714f2ef2183ec1b
Scott Feister <sfeister@gmail.com>
2016-01-07 18:24:02 -0500

    Updated field stitch outputs, frequency analysis

--- h5stitch2D.py
+++ h5stitch2D.py
@@ -12,6 +12,8 @@ import h5py
 import os
 import lspreader2 as rd
 import numpy as np
+import re
+import gzip
 
 try:
     from mpi4py import MPI
@@ -79,6 +81,169 @@ def chunkIt(seq, num):
 
     return out
 
+def h5fields2D(folder, h5path=None, fld_ids = ['Ex','Ey','Ez','Bx','By','Bz']):
+    """ 100% serial processing, but parallel processing is available. Creates the HDF5 file without all the pain. """
+    if not h5path:
+        h5path = os.path.join(folder, 'fields2D.hdf5')
+    fns = getfnsp4(folder)
+    nfiles = len(fns)
+    print 'Total number of files:', len(fns)
+
+    print "Opening the HDF5 file"
+    with h5py.File(h5path,'w') as f:
+        # Read all the files into RAM
+        print "Reading files into NumPy arrays in RAM"
+        data = fields2D(fns, fld_ids=fld_ids)
+        # Build the HDF5 file, assuming every element in "data" is a NumPy array
+        print "Saving arrays in RAM to HDF5"
+        for k in data:
+            f.create_dataset(k, data = data[k], compression='gzip', compression_opts=4)
+    print "All done!"
+
+def h5fields2Dpar2(folder, h5path=None, fld_ids = ['Ex','Ey','Ez','Bx','By','Bz']):
+    """ Parallel processing. DOES NOT WORK IN CURRENT FORM. Creates the HDF5 file with less pain. However, you need lots of RAM on node0 to hold all the data."""
+    if not h5path:
+        h5path = os.path.join(folder, 'fields2D.hdf5')
+    fns = getfnsp4(folder)
+    nfiles = len(fns)
+
+    # Do some fancy parallel stuff
+    nprocs = MPI.COMM_WORLD.Get_size()
+    rank = MPI.COMM_WORLD.Get_rank()
+    name = MPI.Get_processor_name()
+
+    comm = MPI.COMM_WORLD
+
+    # Split the files across nodes
+    if rank == 0:
+        fns_chunked = chunkIt(fns, nprocs) # Break the list into N chunks, where N = number of processors
+        print "Scattering " + str(nfiles) + " fld*.p4 files across " + str(nprocs) + " nodes, to read into NumPy arrays and pass back."
+    else:
+        fns_chunked = None
+    
+    fns_part = comm.scatter(fns_chunked,root=0) # Assign each processor one chunk of the files. Therefore, each processor can look at fns_part (partial list of filenames) as a list of files for it to analyze.
+
+    # Read files assigned to this processor into RAM
+    data_part = fields2D(fns_part, fld_ids=fld_ids)
+    # Perform any additional analysis
+
+    ## Consolidate data on the 0th core to copy everything into a compressed dataset.      
+    data_chunked = comm.gather(data_part, root=0)
+
+    
+    print data_chunked[0]
+    if rank == 0:
+        print "Data read-in complete, and data transferred to proc. 0. Stitching into HDF5 on proc. 0."
+        
+        with h5py.File(h5path,'w') as f:
+            print "HDF5 file opened."
+            # Build the HDF5 file, assuming every element in "data" is a NumPy array
+            print "Saving arrays in RAM to HDF5"
+
+            # Choose a non-empty chunk to extract keys
+            count = 0
+            while (len(fns_chunked[count]) < 1):
+               print 'The count is:', count
+               count = count + 1
+            print "Chose test idx of", count
+
+            data_test = data_chunked[count]
+            # Pre-allocate HDF5 arrays
+            nostretch = ['xgv', 'ygv', 'zgv'] # keys of arrays that should not scale with the filenumber (array built from one file)
+            stretch = [] # keys of arrays that will scale with the filenumber (array built from many files); built in next loop
+            for k in data_test:
+                shape = data_test[k].shape
+                if k in nostretch:
+                    #print 'Will not stretch to number of files: ', k
+                    shape_new = shape
+                else:
+                    stretch.append(k)
+                    #print 'Will stretch to number of files:', k
+                    shape_list = list(shape)
+                    shape_list[0] = nfiles # resize the test shape to account for all files
+                    shape_new = tuple(shape_list)
+                print k, ":", shape, " resized=> ", shape_new
+                f.create_dataset(k, shape_new, dtype=data_test[k].dtype, compression='gzip', compression_opts=4)
+
+            # Fill in the HDF5 arrays
+            nsaved = 0
+            for i in range(nprocs):
+                nfiles = len(fns_chunked[i])
+                print nfiles, "files and "
+                for j in range(nfiles):
+                    print 'J', j, 'J + nsaved', j + nsaved
+                    for k in stretch:
+                        print k
+                        print data_chunked[i][k].shape, f[k].shape
+                        if k == 'times':
+                            print 'Time', data_chunked[i][k][j], '===>', f[k][j + nsaved]
+                            print k, data_chunked[i][k].shape, '===>', f[k].shape
+                        else:
+                            print k, data_chunked[i][k].shape, '===>', f[k].shape
+                        f[k][j + nsaved] = data_chunked[i][k][j]
+                        print k, data_chunked[i][k].shape, '===>', f[k].shape
+                    print "Saved one whole file!"
+                nsaved = nsaved + nfiles
+                print "Finished data for processor", i, "Have now saved", nsaved, "files."
+
+            print "All done!"
+
+def getTimes(fns):
+    """ Get a list of times (in ns), from a list of .p4(.gz) filenames. """
+
+    nfiles = len(fns)
+    times = np.zeros(nfiles)    
+    for i in range(nfiles):
+        fn = fns[i]
+        _, ext = os.path.splitext(fn)
+        
+        if ext == '.gz':
+            with gzip.open(fn, 'rb') as f:
+                header = rd.get_header(f)
+        else:
+            with open(fn, 'rb') as f:
+                header = rd.get_header(f)
+        times[i] = header['timestamp']
+
+    return times
+
+def fields2D(fns, fld_ids = ['Ex','Ey','Ez','Bx','By','Bz']):
+    """ Read in the flds*.p4(.gz) files in the list fns, stitching them together assuming 2D assumptions, and create output arrays """
+    # The first dimension of each array must be nfiles long.
+    
+    ## Extract "E" from "Ex" in fld_ids (for rd.read_flds2() call later)    
+    flds = list(set(s[:-1] for s in fld_ids)) # we strip the "x","y","z" last character off our fields, then set() gives only unique elements of a list, and list() converts this set back to list
+    # E.g. if fld_ids = ['Ex','Ey','Ez','Bx','By','Bz'], flds = ['E','B']
+
+    nfiles = len(fns) # Count the number of files we need to read
+
+    ## Read in the first file as a template
+    doms, header = rd.read_flds2(fns[0], flds=flds)
+    
+    ## Pre-allocate the NumPy arrays inside an output dict called 'data'
+
+    data = {} # define 'data' as a python dictionary that will store all the data read in, including fields, as NumPy arrays    
+    _, xgv, zgv = rd.stitch2D(doms, fld_ids[0]) # Extract the interesting fields and stitch together for a template
+    data['times'] = np.zeros((nfiles,))
+    data['xgv'] = xgv
+    data['zgv'] = zgv
+    data['filenames'] = np.array(fns)
+
+    for k in fld_ids:
+        data[k] = np.zeros((nfiles,len(zgv),len(xgv))) # The vector field elements
+
+    # Read in all the data and fill up the NumPy arrays
+    for i in range(nfiles): # Iterate over the files
+        print "Reading file", i, "of", nfiles        
+        fn = fns[i]
+        doms, header = rd.read_flds2(fn, flds=flds)
+        data['times'][i] = header['timestamp']
+        for k in fld_ids: # Iterate over the requested fields, stitching then adding them to fldDict arrays
+            fld, _, _ = rd.stitch2D(doms, k)
+            data[k][i,:,:] = fld
+   
+    return data
+
 def h5fields2Dser(folder, h5path=None, fld_ids = ['Ex','Ey','Ez','Bx','By','Bz']):
     """ 100% serial processing. Read in a folder full of flds*.p4(.gz) files, stitching them together assuming 2D assumptions, and create an HDF5 file """
     
@@ -142,6 +307,7 @@ def h5fields2Dpar(folder, h5path=None, fld_ids = ['Ex','Ey','Ez','Bx','By','Bz']
         idx_chunked = None
     
     idx_part = comm.scatter(idx_chunked,root=0) # Assign each processor one chunk of the list. Therefore, idx_part (list of indices) specifies which elements to analyze here.
+
 
     # Read in all the data and create temporary lists, 'times' and 'flds'
     times_part = []
