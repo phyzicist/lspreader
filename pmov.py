d793ac2d4bc480113464b9126b8c37ad69e2418e
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2016-01-07 22:37:21 -0500

    made experimental hasher reader script--ready for use

--- pmov.py
+++ pmov.py
@@ -12,8 +12,8 @@ Options:
     --sort -s         Sort the pmovies by IPP.
     --hdf -H          Output to hdf5 instead of to a pickle file.
                       The group will be based on the step.
-    --zip -z          Use compression for hdf5.
-    --npz -n          Use numpy.savez to save.
+    --zip -c          Use compression for hdf5.
+    --npy -n          Use numpy.save to save.
     --verbose -v      Be verbose.
     --lock=L -l L     Specify a lock file for synchronized output for hdf5.
     --exp-d=DFILE     Experimental hashing. It might work only for uniform
@@ -22,6 +22,10 @@ Options:
     --exp-first=DFILE Experimental hashing, see above. Use this file as the
                       first file to generate the hash specification from and
                       output to DFILE.
+    --X -x            Use X as a spatial dimension. Similar options below are
+                      for Y and Z. If none are passed, assume 3D cartesian.
+    --Y -y            See above.
+    --Z -z            See above.
 '''
 import lspreader as rd;
 import h5py as h5;
@@ -133,7 +137,7 @@ def addhash(frame,d,removedupes=False):
     '''
     hashes = genhash(frame,d,removedupes);
     frame['data'] = rfn.rec_append_fields(
-        'hash',hashes,frame['data']);
+        frame['data'],'hash',hashes);
     return frame;
 
 #script start. This garbage to let people like scott use
@@ -141,7 +145,12 @@ def addhash(frame,d,removedupes=False):
 if __name__=='__main__':
     opts = docopt(__doc__,help=True);
     vprint = mkvprint(opts);
-
+    dims=[]
+    if opts['--X']: dims.append('x');
+    if opts['--Y']: dims.append('y');
+    if opts['--Z']: dims.append('z');
+    if len(dims)==0:
+        dims=['x','y','z'];
     #reading in using the reader.
     frames=rd.read(opts['<input>']);
     
@@ -151,7 +160,7 @@ if __name__=='__main__':
         vprint("done");
     #experimental hashing
     if opts['--exp-first']:
-        d=firsthash(frames[0]);
+        d=firsthash(frames[0],dims);
         dump_pickle(opts['--exp-first'], d);
         frames[:] = [addhash(frame,d) for frame in frames];
     elif opts['--exp-d']:
@@ -173,8 +182,8 @@ if __name__=='__main__':
             np.save(opts['<output>'], [frame['data'] for frame in frames]);
     else:
         if not opts['<output>']:
-        for frame in frames:
-            outname = "{}.{}".format(opts['<input>'],frame['step']);
-            dump_pickle(outname, frame);
+            for frame in frames:
+                outname = "{}.{}".format(opts['<input>'],frame['step']);
+                dump_pickle(outname, frame);
         else:
             dump_pickle(opts['<output>'], frames);
--- pmovie-reader/pmovie-pbsgen-exp.py
+++ pmovie-reader/pmovie-pbsgen-exp.py
@@ -32,9 +32,11 @@ def call(cmd):
 ls = call(('ls',workdir))
 # filtering for pmovies
 pmovierx=re.compile(r"pmovie([0-9]+).p4$");
-lspmovies = [(s,int(pmovierx.match(s).group(1))) for s in ls if pmovierx.match(s)];
+lspmovies = [(s,int(pmovierx.match(s).group(1)))
+             for s in ls if pmovierx.match(s)];
 drx = re.compile(r"pmovie([0-9]+).p4.d$");
-lsds = [int(drx.match(s).group(1)) for s in ls if drx.match(s)];
+lsds = [int(drx.match(s).group(1))
+        for s in ls if drx.match(s)];
 #O(N^2), although N is only ~2000, we can do better.
 lspmovies = [i for i in lspmovies if i[1] not in lsds];
 lspmovies.sort(key=lambda i: i[1]);
@@ -42,12 +44,28 @@ if len(ls) == 0:
     print("I see no pmovies, exiting.");
     exit(1);
 filesstr = [i[0] for i in lspmovies];
+dotlsprx = re.compile(r".*\.lsp");
+dotlsp = [s for s in ls if dotlsprx.match(s)][0];
+#reading .lsp file to find information
+with open(dotlsp,'r') as f:
+    dotlsp = f.readlines();
+#Figure out the dimensionality of the simulation.
+#Another possiblity is to read the output dir which
+#has compiler flags. This will work for now as long
+#as the .lsp file is cleanish. Basically search for the
+#Grid and look for the *-cells entries
+cellrxs=[re.compile(r"^{}-cells +[0-9]+".format(dim))
+         for dim in ['x','y','z']]
+dims =  [ np.array([rx.match(line) for line in dotlsp]).max()
+          for rx in cellrxs ]
+dims = [dim for dim,isdim in zip(['x','y','z'],dims) if isdim];
+dims_flag = ' -{} '.format(''.join(dims));
 
 #providing for different settings which varies
 #by server.
 server_settings = {
     #           ppn  hours  mins
-    'ramses': [48,   999,      0],
+    'ramses': [44,   999,      0],
     'glenn':  [ 8,    48,      0]
 };
 try:
@@ -72,15 +90,15 @@ template='''
 
 source $HOME/.bashrc
 source $HOME/conda
-LOGFILE=pmovie-conv-{post}.log
+LOGFILE=$PBS_O_WORKDIR/pmovie-conv-{post}.log
 cd {workdir}
 >$LOGFILE
 echo "processing first file...{firstfile}">>$LOGFILE
-./pmov.py --exp-first=./hash.d {opts} {firstfile} {outfile}&>>$LOGFILE
+{firstfile}
 for i in {filelist}; do
     while [ $(pgrep -f pmov.py  |  wc -l ) -eq {ppn} ]; do sleep 10; done; 
     echo "running $i">>$LOGFILE
-    ./pmov.py {opts} --exp-d=./hash.d $i {outfile}&>>$LOGFILE&
+    ./pmov.py {opts} -n --exp-d=./hash.d {outfile}&>>$LOGFILE&
 done
 
 while [ $(pgrep -f pmov.py | wc -l) -gt 0 ]; do
@@ -89,12 +107,11 @@ while [ $(pgrep -f pmov.py | wc -l) -gt 0 ]; do
     sleep 10;
 done
 '''
-pbsouts=[];
 
+firstfiletmpl="./pmov.py -n --exp-first=./hash.d {opts} {firstfile} {outfile}&>>$LOGFILE"
 for i,f in enumerate(pbses):
     post = postfmt.format(i);
-    files = ' '.join(f);
-    xopts=''
+    xopts=''+dims_flag; #for copy so we don't write to dims_flag
     #handle hdf output
     if opts['--hdf']:
         xopts+=' -H '
@@ -106,15 +123,24 @@ for i,f in enumerate(pbses):
         ramsesnode=':'+opts['--ramses-node'];
     else:
         ramsesnode='';
+    if i==0:
+        firstfile = firstfiletmpl.format(
+            opts=xopts,
+            firstfile=f[0],
+            outfile=outfile);
+        files=' '.join(f[1:]);
+    else:
+        firstfile='';
+        files = ' '.join(f);
     out = template.format(
         hours=hours,
         mins=mins,
         ppn=ppn,
-        ramsesnode=ramsesnode
+        ramsesnode=ramsesnode,
         post=post,
         workdir=workdir,
-        firstfile=files[0],
-        filelist=files[1:],
+        firstfile=firstfile,
+        filelist=files,
         opts=xopts+opts['--extra-opts'],
         outfile=outfile);
     with open(opts['--outdir']+'/pmovie-conv-'+post+'.pbs','w') as f:
