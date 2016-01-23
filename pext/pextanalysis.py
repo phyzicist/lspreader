Analysis improvements, and units incorrectly labeled in quantities.py
Quantities.py had units labeled in LSP units but they are actually input
in SI

pext/pextanalysis.py
@@ -28,15 +28,19 @@ import numpy as np
 import os
 
 import angular # Radial histogram plot, from Gregory's local file 'angular.py'
+import quantities
 import lspreader2 as rd # Hope this works!
 import lstools as ls # local
+import sftools as sf # local
+import scipy.constants as sc
 
-def pextFull(p4dir, outdir = '.', shortname = 'Sim'):
+def pextFull(p4dir, outdir = '.', shortname = 'Sim', Utot_Jcm = None):
     """ Perform the full-blown analysis of pext*.p4(.gz) files from a simulation. 
     Inputs:
         p4dir: string, path to the folder which contains the pext*.p4
         outdir: string, path to an already-existent folder in which to save outputs (PNG plots)
         shortname: string, a fairly short identifier for the simulation which will be added into plots and filenames
+        Utot_Jcm: number (optional): If total simulation energy is given (in J/cm, for a 2D sim), then relative efficiencies will be plotted and written to CSV.
     Outputs:
         Saves a bunch of well-labeled PNG plot files into the folder 'outdir'
     """
@@ -46,7 +50,7 @@ def pextFull(p4dir, outdir = '.', shortname = 'Sim'):
         msg = "No pext*.p4(.gz) files found in the directory: " + p4dir
         raise Exception(msg)
     pextarr = loadPexts(fns)
-    plotme(pextarr, outdir=outdir, shortname=shortname) # Plots and saves
+    plotme(pextarr, outdir=outdir, shortname=shortname, Utot_Jcm = Utot_Jcm) # Plots and saves
     
 def loadPexts(fns):
     """ Calls lspreader2 to load all the pext*.p4 from a given list of filenames, and concatenates into a single array
@@ -65,7 +69,7 @@ def loadPexts(fns):
             pextarr = np.concatenate((pextarr, pextarr_tmp), 0) # Append this pext*.p4 'out' to the output array 'outs'
     return pextarr
 
-def plotme(pextarr, outdir='.', shortname='Sim', mksub=False):
+def plotme(pextarr, outdir='.', shortname='Sim', mksub=False, Utot_Jcm = None):
     """
     Make Scott's preferred set of plots (and save to file) from the output (or concatenated outputs) of lspreader "pext()" of pext.
     Inputs:
@@ -73,6 +77,7 @@ def plotme(pextarr, outdir='.', shortname='Sim', mksub=False):
         outdir: string, path to a folder (already existent) that can hold the outputs
         shortname: string, a fairly short identifier for the simulation which will be added into plots and filenames
         mksub: bool, if True then make a subdirectory in 'outdir', otherwise just dump the PNGs directly into 'outdir'
+        Utot_Jcm: number (optional): If total simulation energy is given (in J/cm, for a 2D sim), then relative efficiencies will be plotted.
     Outputs:
         Will save PNG figures summarizing the extraction planes into the folder specified by 'outdir'.
     
@@ -82,7 +87,7 @@ def plotme(pextarr, outdir='.', shortname='Sim', mksub=False):
 
     # Make a subdirectory for the PNG files, if mksub was set to True
     if mksub:
-        plotdir = subdir(outdir, 'Extraction planes')
+        plotdir = sf.subdir(outdir, 'Extraction planes')
     else:
         plotdir = outdir
         
@@ -90,17 +95,42 @@ def plotme(pextarr, outdir='.', shortname='Sim', mksub=False):
     massE = 0.511 # electron rest mass, in MeV
     d = {}
     d['t'] = pextarr['t']*1e6 # Time, in fs
-    u_norm = np.sqrt(pextarr['ux']**2+pextarr['uy']**2+pextarr['uz']**2) # 'uz', etc. are actually momenta; in what they call gamma-beta units, so unitless and related only to speed of light
-    d['KE'] =(np.sqrt(u_norm**2+1)-1)*massE # Electron kinetic energy, in MeV
+    u_norm = np.sqrt(pextarr['ux']**2+pextarr['uy']**2+pextarr['uz']**2) # 'uz', etc. is actually momentum / (speed of light * rest mass); it's what they call gamma-beta units; that is, ux = px/c/mass_0 = gamma * beta_x
+    #p_norm = massE * u_norm # The momenta of the particles, 
+    d['KE'] =(np.sqrt(u_norm**2+1)-1)*massE # Electron kinetic energy, in MeV (kinetic energy of each constituent real electron, that is; not of the whole macroparticle)
     d['q'] = -pextarr['q']*1e3 # Amount of negative charge, nanoColoumbs/cm
     d['phi'] = np.arctan2(pextarr['uz'], pextarr['ux'])
-        
+    
+    m_macro = -((pextarr['q']*1e-6)/sc.e)*sc.m_e # Mass of this macroparticle, in kg/cm. Mass macroparticle = (Coulombs macroparticle / coulombs electron) * mass electron
+    d['KE_macro_Jcm'] = (m_macro * sc.c**2)*(np.sqrt(u_norm**2 + 1) - 1) # Kinetic energy of this macroparticle, in J/cm
+    
+    if Utot_Jcm: # If the user input a value for total energy
+        ## CSV: Write some basic efficiency info to file
+        with open(os.path.join(outdir, shortname + ' - Electron Efficiency.csv'), 'w') as f:
+            f.write('Total energy in sim: ' + str(Utot_Jcm) + " J/cm\n")
+            f.write("MINIM_ELECTRON_MEV, PERCENT_EFFICIENCY_PLUSMINUS_40DEG, PERCENT_EFFIC_PLUSMINUS_6point3_DEG\n")        
+            ecuts = [0.12, 0.3, 0.5, 1.0, 1.5, 3] # Cutoffs for energy efficiency, in MeV
+            for ecut in ecuts:
+                cdtA = np.logical_and(d['KE'] > ecut, np.abs(d['phi']) > np.deg2rad(180 - 40))
+                cdtB = np.logical_and(d['KE'] > ecut, np.abs(d['phi']) > np.deg2rad(180 - 6.3))
+                Ue_JcmA = np.sum(d['KE_macro_Jcm'][cdtA]) # Energy of the electrons meeting this condition, in J/cm
+                Ue_JcmB = np.sum(d['KE_macro_Jcm'][cdtB]) # Energy of the electrons meeting this condition, in J/cm
+                efficA = Ue_JcmA/Utot_Jcm
+                efficB = Ue_JcmB/Utot_Jcm
+                f.write(str(ecut) + ", " + str(np.round(efficA*100,2)) + ", " + str(np.round(efficB*100,2)) + "\n")
+            cdt120 = np.logical_and(d['KE'] > 0.120, np.abs(d['phi']) > np.deg2rad(180 - 40))
+            effic120 = np.sum(d['KE_macro_Jcm'][cdt120])/Utot_Jcm
+            efficstr = r'$\stackrel{>120 keV}{\pm 40^{\circ}}$ Efficiency: ' + str(np.round(effic120 * 100, 2)) + "%"
+    else:
+        efficstr = '' # Implying that efficiency was not calculated.
+    
     ## RADIAL PLOT WITHOUT CUTOFFS
     fig = plt.figure(3)
     plt.clf()
     title=r'Radial electron spectrum'
     angular.angular2(d['q'], d['phi'], d['KE'], fig = fig, cmap='viridis', minQ=0, maxQ=1.5, title=title)
-    plt.figtext(0.99, 0.01, shortname, horizontalalignment='right')
+    plt.figtext(0.99, 0.02, shortname, horizontalalignment='right')
+    plt.figtext(0.01, 0.02, efficstr, horizontalalignment='left')
     fig.savefig(os.path.join(plotdir, shortname + ' - Radial.png'))
    
     ## RADIAL PLOT WITH CUTOFF
@@ -110,7 +140,8 @@ def plotme(pextarr, outdir='.', shortname='Sim', mksub=False):
     #title = shortname + r": 300/40 charge: " + str(round(np.sum(d['q'][condit2])))
     title=r'Radial electron spectrum, $\pm$40$^{\circ}$'
     angular.angular2(d['q'][condit2], d['phi'][condit2], d['KE'][condit2], fig = fig, cmap='viridis', minQ=0, maxQ=1.5, title=title)
-    plt.figtext(0.99, 0.01, shortname, horizontalalignment='right')
+    plt.figtext(0.99, 0.02, shortname, horizontalalignment='right')
+    plt.figtext(0.01, 0.02, efficstr, horizontalalignment='left')
     fig.savefig(os.path.join(plotdir, shortname + ' - Radial with cutoffs.png'))
     
     ## BACKWARD ELECTRON SPECTRUM LINE PLOT
@@ -267,10 +298,3 @@ def plotme(pextarr, outdir='.', shortname='Sim', mksub=False):
     fig.savefig(os.path.join(plotdir, shortname + ' - Zoomed TOF.png'))
 
     plt.close('all') #Close all the figures we just opened to avoid hogging memory
-
-def subdir(folder, name):
-    """ Make a subdirectory in the specified folder, if it doesn't already exist"""
-    subpath = os.path.join(folder,name)
-    if not os.path.exists(subpath):
-        os.mkdir(subpath)
-    return subpath

scottplots.py
@@ -328,6 +328,141 @@ def plotDens(data, outdir='.', shortname = '', alltime=False):
      ## Close the plots
     plt.close('all')
 
+
+def bsFFT(data, trange = (80, 1.0e9)):
+    """ A wrapper for emFFT() for the case of Backscatter analysis. Outputs are lines (rather than maps) and are in plotting units.
+    Inputs:
+        data: the usual data dict
+        trange: (fs) the min and max time to look at for this "summing energy through the plane" analysis
+    Outputs:
+        lines: dict containing multiple 1D arrays, total energy as a function of Z position, cut by frequency, in units of "mJ/um^2"
+        cuts: dict with same keys as lines, but containing the frequency cuts (in units of the fundamental) corresponding to lines
+        pwrsum: 1D array, power spectrum Y, units are mJ/um/(freq. fund)
+        freq: 1D array, power spectrum X, units are frequency (in units of the fundamental)
+        Utots: dict with same keys as lines, but with each key giving a single number (the total energy at that frequency, in mJ/um)
+    """
+    # Assume equal spacing in time and space, and get the deltas
+    dt = np.mean(np.diff(data['times']))*1e-9 # dt in seconds
+    dx = np.mean(np.diff(data['xgv']))*1e-2 # dx in m
+    dz = np.mean(np.diff(data['zgv']))*1e-2 # dx in m
+
+    # Do the Fourier analysis
+    maps, cuts, pwrsum, freq, df, nframes = emFFT(data, kind='Backscat', trange=trange)
+    
+    # All these energies returned by emFFT are the mean energy value, across the time period; if we want the sum, we need to multiply by number of frames analyzed
+    # But there is now another problem. We have overcounted the energy by a factor of (dx*c/dt), because while dx should be c/dt long, it is actually larger and a delta pulse of light gets counted multiple frames in a row.
+    pwrline = pwrsum*(nframes * sc.c * dt/dx) # Units are now J per y meter per freq. (integrated rather than averaged over all time)
+    lines = {}
+    for k in maps:
+        lines[k] = np.squeeze(maps[k]*(nframes * sc.c * dt/dx)) # Units are now Joules per y meter per z meter (integrated rather than averaged over all time). Not really, I can't figure out what's wrong though.
+    
+    # That's better!
+
+    print lines['0'].shape
+    
+    
+    print "Total energy, in mJ/micron:", np.sum(lines['all'])*dx*dz*1e-3 
+    print "Total energy, in mJ/micron:", np.sum(pwrline)*df*1e-3
+    
+    ## UNIT CONVERSION FROM SI to PLOTTING UNITS (mJ/micron, etc.)
+    for k in lines:
+        lines[k] = lines[k]*dx*1e-9 # units of lines (Energy in frequencies / Z axis) are now in mJ / z micron / y micron
+    pwrline = pwrline * 1e-3 # pwrline (Power spectrum Y axis) is now in of mJ / freq. unit / y micron
+    
+    dt = dt * 1e15 # dt is now in fs
+    dx = dx * 1e6 # dx is now in microns
+    dz = dz * 1e6 # dz is now in microns
+    
+    Utots = {}
+    for k in lines:
+        Utots[k] = np.sum(lines[k])*dz # Total energy in the frequency band, in mJ/ y micron
+    return lines, cuts, pwrline, freq, Utots
+    
+def plotBScat(data, tcut=80, outdir='.', shortname = ''):
+    """ Plot relevant backscattered light plots. Energy calculation makes some iffy assumptions about angles of light, so some degree of systematic error.
+    Inputs:        
+        data: the usual data dict
+        tcut: number, a time (fs) that splits forward light from backward. I.e. after this time (in fs), we consider it backscattered light. Before this time, we consider it incident light.
+        outdir: directory where PNG plots will be saved
+        shortname: a short string describing the run, e.g. 'a40f-14_so'
+    """
+    linesT, _, pwrlineT, freqT, U_T = bsFFT(data, trange=(0,tcut)) # Input light (T)ransmission characteristics
+    linesR, cuts, pwrlineR, freqR, U_R = bsFFT(data, trange=(tcut,1.0e9)) # Backscatter (R)eflection characteristics
+    
+    zgv = data['zgv']*1e4 # Z grid vector, in microns
+    
+    R = U_R['all']/U_T['all'] # Reflectivity, roughly
+    print "Reflectivity: " + str(np.round(R*100, 2)) + "%"
+    Rstring = '(R = ' + str(np.round(R*100,2)) + '%)'
+    
+    ## CSV: Write some reflectivity info to file.
+    with open(os.path.join(outdir, shortname + ' - Reflectivity.csv'), 'w') as f:
+        f.write("FREQ_BAND, PERCENT_ENERGY_IN_REFLECTED, FREQ_CUT1, FREQ_CUT2, TRANSMITTED_MILLIJOULE_PER_MICRON, REFLECTED_MILLIJOULE_PER_MICRON\n")        
+        for k in U_T:
+            f.write(k + ", " + str(np.round(U_R[k]/U_T['all']*100, 4)) + ", " + str(cuts[k][0]) + ", " + str(cuts[k][1]) + ", " + str(U_T[k]) + ", " + str(U_R[k]) + "\n")
+
+    ## Plot 1: Backscatter power spectrum
+    title = 'Backscattered light power spectrum ' + Rstring
+    fld_id = r'$L$'
+    fig = plt.figure(1)
+    plt.clf() # Clear the figure
+    ymax = 100
+    ymin = 10**(np.log10(ymax) - 4) # Give 4 orders of magnitude
+    xmin = 0
+    xmax = 2.5
+    ax = plt.subplot(111)
+    ax.plot(freqT, pwrlineT, '-.k', label='T')
+    ax.plot(freqR, pwrlineR, '-k', label='R')
+    ax.set_xlabel('Angular frequency / $\omega_{laser}$', fontsize=20)
+    ax.set_ylabel('Energy passing plane ($mJ / \omega_{laser} / \mu m$)')
+    ax.legend(ncol=1, loc=1, borderaxespad=0.)
+    ax.set_yscale('log')
+    plt.xticks(fontsize=14)
+    ax.set_title(title, fontsize=16)
+    ax.set_xlim(xmin, xmax)
+    ax.vlines([0.5,1.0,1.5,2.0], -1.0e30, 1.0e30, colors=[(0.6,0,0),'r','g','b'], linestyle=':') # vertical lines, colored by frequency
+    ax.yaxis.grid(which='minor', color='0.92', linestyle='--') # horizontal lines
+    ax.yaxis.grid(which='major', color='0.92', linestyle='-') # horizontal lines
+    ax.set_axisbelow(True) # Move the grid marks under the axes
+    ax.text(0.9 ,0.05, fld_id, fontsize=44, color='black', transform=ax.transAxes) # http://matplotlib.org/api/axes_api.html#matplotlib.axes.Axes.text
+    ax.set_ylim(ymin, ymax)
+    fig.text(0.99, 0.01, shortname, horizontalalignment='right')
+    fig.savefig(os.path.join(outdir, shortname + ' - BScat Spectrum.png'))
+
+    ## Plot 2: Backscatter power spectrum
+    title = 'Backscattered light vs. Z axis ' + Rstring
+    fld_id = r'$L$'
+    fig = plt.figure(2)
+    plt.clf() # Clear the figure
+    ymax = 1
+    ymin = 10**(np.log10(ymax) - 4) # Give 4 orders of magnitude
+    xmin = np.min(zgv)
+    xmax = np.max(zgv)
+    ax = plt.subplot(111)
+    ax.plot(zgv, linesT['all'], '-.k', label='T')
+    ax.plot(zgv, linesR['all'], '-k', label='R')
+    ax.plot(zgv, linesR['0_5'], color=(0.6,0,0), label='$\omega/2$')
+    ax.plot(zgv, linesR['0_8'], '-.r', label='$.8\omega$')
+    ax.plot(zgv, linesR['1_0'], 'r', label='$\omega$')
+    ax.plot(zgv, linesR['1_5'], 'g', label='$3\omega/2$')
+    ax.plot(zgv, linesR['2_0'], 'b', label='$2\omega$')
+    ax.legend(ncol=1, loc=1, borderaxespad=0.)
+    ax.set_xlabel('Z ($\mu m$)', fontsize=20)
+    ax.set_ylabel('Energy at Z position ($mJ / \mu m^2$)')
+    ax.set_yscale('log')
+    ax.set_title(title, fontsize=16)
+    ax.xaxis.grid(which='both', color='0.92', linestyle='-') # vertical lines
+    ax.yaxis.grid(which='both', color='0.87', linestyle='--') # horizontal lines
+    ax.set_axisbelow(True) # Move the grid marks under the axes
+    ax.set_ylim(ymin, ymax)
+    ax.set_xlim(xmin, xmax)
+    ax.text(0.9 ,0.05, fld_id, fontsize=44, color='black', transform=ax.transAxes)
+    fig.text(0.99, 0.01, shortname, horizontalalignment='right')
+    fig.savefig(os.path.join(outdir, shortname + ' - BScat vs Z.png'))
+    
+    plt.close('all') # Close all the figures
+
+
   
 def plotEM(data, outdir='.', shortname = '', alltime=False):
     """ Make Scott's set of custom electromagnetic field (and frequency) plots for this batch """
@@ -433,4 +568,3 @@ def plotEM(data, outdir='.', shortname = '', alltime=False):
     
     ## Close the plots
     plt.close('all')
-